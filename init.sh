045e07ab528ad2c1185f794fa76d5723b8b14c99
Gabor Szarnyas <szarnyasg@gmail.com>
2022-03-06 16:25:56 +0100

    Fix path

--- init.sh
+++ init.sh
@@ -5,10 +5,10 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 ./mount-disk.sh
 
-tmux new-session -s ldbc -n etc -d '~/bootstrap-ec2 && ./init-packages.sh; bash -i'
+tmux new-session -s ldbc -n etc -d '~/ec2-bootstrap/ && ./init-packages.sh; bash -i'
 tmux split-window -h 'htop'
 tmux select-pane -L
-tmux split-window -v 'cd ~/bootstrap-ec2/ && ./init-datagen.sh; bash -i'
-tmux split-window -v 'cd ~/bootstrap-ec2/ && ./init-bi.sh; bash -i'
+tmux split-window -v 'cd ~/ec2-bootstrap/ && ./init-datagen.sh; bash -i'
+tmux split-window -v 'cd ~/ec2-bootstrap/ && ./init-bi.sh; bash -i'
 
 tmux -2 attach-session -t ldbc
