8ef6766352c77986bea0617a216a67f42a18a613
Gabor Szarnyas <szarnyasg@gmail.com>
2022-03-06 12:27:07 +0100

    Fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # prompt
-<<EOF > ~/.bashrc
+cat << EOF >> ~/.bashrc
 parse_git_branch() {
      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
@@ -9,7 +9,7 @@ export PS1="\u@\h \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
 EOF
 
 # gitconfig
-<<EOF > ~/.gitconfig
+cat << EOF >> ~/.gitconfig
 [core]
 	editor = vim
 	quotepath = false
