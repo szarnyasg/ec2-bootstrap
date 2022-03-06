8c3847b3b9cad0048bef3fbd64d3a350e8aafebf
Gabor Szarnyas <szarnyasg@gmail.com>
2022-03-06 18:29:47 +0100

    escape

--- datagen-env-vars.sh
+++ datagen-env-vars.sh
@@ -2,7 +2,7 @@
 
 cat << EOF >> ~/.bashrc
 export SPARK_HOME=/opt/spark-3.1.2-bin-hadoop3.2
-export PATH="$SPARK_HOME/bin":"$PATH"
+export PATH="\$SPARK_HOME/bin":"\$PATH"
 export PLATFORM_VERSION=2.12_spark3.1
 export DATAGEN_VERSION=0.5.0-SNAPSHOT
 export LDBC_SNB_DATAGEN_DIR=/ldbc/ldbc_snb_datagen_spark
