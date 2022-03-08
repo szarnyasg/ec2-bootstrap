1d5d47e870b864ef8c88d9be37ac045e7a6eb9a3
Gabor Szarnyas <szarnyasg@gmail.com>
2022-03-08 10:40:19 +0100

    tmuxconf

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,6 +10,17 @@ parse_git_branch() {
 export PS1="\u@\h \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
 EOF
 
+# tmux
+cat << EOF >> ~/.tmux.conf
+set -g default-command "${SHELL}"
+set-option -g history-limit 30000
+
+# Set new panes to open in current directory
+bind c new-window -c "#{pane_current_path}"
+bind '"' split-window -c "#{pane_current_path}"
+bind % split-window -h -c "#{pane_current_path}"
+EOF
+
 # gitconfig
 cat << EOF >> ~/.gitconfig
 [core]
