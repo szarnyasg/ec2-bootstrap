c3aa0575b6765c160c7c002a76b38fe87c65eca0
Gabor Szarnyas <szarnyasg@gmail.com>
2022-03-12 20:41:44 +0100

    Fix AWS script

--- init-aws.sh
+++ init-aws.sh
@@ -3,5 +3,5 @@
 set -eu
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-export "export BUCKET_NAME=ldbc-snb-datagen-bi-2021-07" >> ~/.bashrc
-export "export AWS_PROFILE=ldbc" >> ~/.bashrc
+echo "export BUCKET_NAME=ldbc-snb-datagen-bi-2021-07" >> ~/.bashrc
+echo "export AWS_PROFILE=ldbc" >> ~/.bashrc
