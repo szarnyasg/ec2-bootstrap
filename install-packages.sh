81754f2b879b1423956f9138963bdae8db0faa0a
Gabor Szarnyas <szarnyasg@gmail.com>
2022-03-13 12:40:04 +0100

    sysstat

--- install-packages.sh
+++ install-packages.sh
@@ -6,4 +6,6 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 sudo dnf upgrade -y
 sudo dnf install -y zstd mc the_silver_searcher nmon fzf sysstat
 
+sudo service sysstat restart
+
 ./install-awscli.sh
