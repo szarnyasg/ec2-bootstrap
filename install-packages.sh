1d411fa8bf28f4125f329d171194947e990f7b8e
Gabor Szarnyas <szarnyasg@gmail.com>
2022-03-15 00:32:34 +0100

    dnf -> yum

--- install-packages.sh
+++ install-packages.sh
@@ -3,7 +3,7 @@
 set -eu
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-sudo dnf upgrade -y
-sudo dnf install -y mc the_silver_searcher nmon fzf sysstat
+sudo yum upgrade -y
+sudo yum install -y mc the_silver_searcher nmon fzf sysstat
 
 sudo service sysstat restart
