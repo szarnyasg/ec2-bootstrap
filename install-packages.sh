65a0856d26f780e96e19d8254d761cdedac81c04
Gabor Szarnyas <szarnyasg@gmail.com>
2022-03-17 15:58:19 +0100

    tree

--- install-packages.sh
+++ install-packages.sh
@@ -4,6 +4,6 @@ set -eu
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 sudo yum upgrade -y
-sudo yum install -y mc the_silver_searcher nmon fzf sysstat
+sudo yum install -y mc the_silver_searcher nmon fzf sysstat tree
 
 sudo service sysstat restart
