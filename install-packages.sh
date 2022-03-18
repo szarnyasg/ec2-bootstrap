af9ed8763ba4b727782f087c6f5a20cd44061fa1
Gabor Szarnyas <szarnyasg@gmail.com>
2022-03-18 13:02:59 +0100

    Add nano

--- install-packages.sh
+++ install-packages.sh
@@ -4,6 +4,6 @@ set -eu
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 sudo yum upgrade -y
-sudo yum install -y mc the_silver_searcher nmon fzf sysstat tree
+sudo yum install -y mc the_silver_searcher nmon fzf sysstat tree nano
 
 sudo service sysstat restart
