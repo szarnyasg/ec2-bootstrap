3e4765fb704ae793e2a5de398c07ab0305cd6f91
Gabor Szarnyas <szarnyasg@gmail.com>
2022-04-01 00:46:25 +0200

    initialize memory

--- set-datagen-env-vars.sh
+++ set-datagen-env-vars.sh
@@ -6,4 +6,5 @@ export PATH="\$SPARK_HOME/bin":"\$PATH"
 export PLATFORM_VERSION=2.12_spark3.1
 export DATAGEN_VERSION=0.5.0-SNAPSHOT
 export LDBC_SNB_DATAGEN_DIR=/data/ldbc_snb_datagen_spark
+export LDBC_SNB_DATAGEN_MAX_MEM=10G
 EOF
