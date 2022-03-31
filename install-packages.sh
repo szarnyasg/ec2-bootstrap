e1fccd6c06f922a5f1aa6f9fec2cacc4a2c69285
Gabor Szarnyas <szarnyasg@gmail.com>
2022-03-31 22:02:48 +0200

    Add pkg

--- install-packages.sh
+++ install-packages.sh
@@ -4,6 +4,6 @@ set -eu
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 sudo yum upgrade -y
-sudo yum install -y mc the_silver_searcher nmon bmon fzf sysstat tree nano m4 make cmake automake gcc gcc-c++ kernel-devel
+sudo yum install -y mc the_silver_searcher nmon bmon fzf sysstat tree nano m4 make cmake automake gcc gcc-c++ kernel-devel bc
 
 sudo service sysstat restart
