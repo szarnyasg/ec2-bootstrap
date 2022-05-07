8fda82c1685e60d4dc520f0dc20594108fe242ac
Gabor Szarnyas <szarnyasg@gmail.com>
2022-05-07 16:29:30 +0200

    create raid disk

--- create-disks.sh
+++ create-disks.sh
@@ -3,17 +3,15 @@
 set -eu
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# mount first NVMe disk
-if [ -b /dev/xvdb ]; then
-    export NVME_DEVICE=/dev/xvdb
-elif [ -b /dev/nvme1n1 ]; then
-    export NVME_DEVICE=/dev/nvme1n1
-elif [ -b /dev/nvme0n1 ]; then
-    export NVME_DEVICE=/dev/nvme0n1
-fi
-sudo mkfs -t ext4 ${NVME_DEVICE}
-sudo mkdir /data/
-sudo mount ${NVME_DEVICE} /data
+NUM_DISKS=$(lsblk | grep ^nvme[^0] | wc -l)
+sudo mdadm --create --verbose /dev/md0 --level=0 --name=MY_RAID --raid-devices=${NUM_DISKS} $(seq -f "/dev/nvme%gn1" 1 ${NUM_DISKS})
+sudo mkfs.ext4 -L MY_RAID /dev/md0
+sudo mdadm --detail --scan | sudo tee -a /etc/mdadm.conf
+sudo dracut -H -f /boot/initramfs-$(uname -r).img $(uname -r)
+sudo mkdir -p /data
+sudo mount LABEL=MY_RAID /data
+sudo bash -c 'echo "LABEL=MY_RAID       /data   ext4    defaults,nofail        0       2" >> /etc/fstab'
+sudo mount -a
 sudo chown -R ${USER}:${USER} /data
 
 # move Docker's root to disk
@@ -21,15 +19,3 @@ sudo systemctl stop docker
 sudo mkdir -p /etc/docker/
 echo '{ "data-root": "/data/docker" }' | sudo tee /etc/docker/daemon.json > /dev/null
 sudo systemctl start docker
-
-# try creating additional disks
-for INDEX in 2 3 4; do
-    export NVME_DEVICE=/dev/nvme${INDEX}n1
-    # only attempt creating disk if block device ${NVME_DEVICE} exists
-    if [ -b ${NVME_DEVICE} ]; then
-        sudo mkfs -t ext4 ${NVME_DEVICE}
-        sudo mkdir /data${INDEX}/
-        sudo mount ${NVME_DEVICE} /data${INDEX}
-        sudo chown -R ${USER}:${USER} /data${INDEX}
-    fi
-done
