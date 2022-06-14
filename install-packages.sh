f8b8979362293ac37a58c6c6e5b072d5e9997f6e
Gabor Szarnyas <szarnyasg@gmail.com>
2022-06-14 14:01:26 -0400

    py3 devel

--- install-packages.sh
+++ install-packages.sh
@@ -4,6 +4,6 @@ set -eu
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 sudo yum upgrade -y
-sudo yum install -y mc the_silver_searcher nmon bmon fzf sysstat tree nano m4 make cmake automake gcc gcc-c++ kernel-devel zip bc exa bat tldr
+sudo yum install -y mc the_silver_searcher nmon bmon fzf sysstat tree nano m4 make cmake automake gcc gcc-c++ kernel-devel zip bc exa bat tldr python3-devel
 
 sudo service sysstat restart
