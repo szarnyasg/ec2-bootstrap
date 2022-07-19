9ca293f83b277a34ea081d89899aa2b76589e283
David Puroja <dpuroja@outlook.com>
2022-07-19 14:37:18 +0200

    Fix route to ec2 instance

--- terraform/compute/main.tf
+++ terraform/compute/main.tf
@@ -3,9 +3,18 @@ resource "aws_instance" "box" {
     instance_type   = var.aws_instance
 
     key_name        = aws_key_pair.deployer.key_name
-    associate_public_ip_address =true
-    # security_groups = ["${aws_security_group.ingress-all-box.id}"]
-    # subnet_id       = "${aws_subnet.subnet-box.id}"
+
+    associate_public_ip_address = true
+
+    availability_zone = var.aws_availability_zone
+    security_groups = ["${aws_security_group.ingress-all-main.id}"]
+    subnet_id       = "${aws_subnet.main.id}"
+
+    tags = {
+        Name = "Box"
+    }
+
+    depends_on = [aws_internet_gateway.main]
 }
 
 resource "aws_key_pair" "deployer" {
--- terraform/compute/variables.tf
+++ terraform/compute/variables.tf
@@ -10,21 +10,19 @@ variable "aws_instance" {
 
 variable "aws_region" {
     type = string
-    description = "AWS Avaibility zone"
+    description = "AWS Region"
 }
 
-variable "aws_availability_region" {
+variable "aws_availability_zone" {
     type = string
     description = "AWS Avaibility zone"
 }
-
 
 variable "aws_profile" {
     type = string
     default = "default"
     description = "AWS Profile name matching the shared credentails"
 }
-
 
 variable "key_pair_name" {
     type = string
@@ -34,4 +32,9 @@ variable "key_pair_name" {
 variable "key_pair_public_key" {
     type = string
     description = "The public key for the key pair (starts with ssh-key and ends with the email address for the public key)"
+}
+
+variable "subnet_cidr_block" {
+    type = string
+    description = "The CIDR subnet block to associate with the VPC"
 }
