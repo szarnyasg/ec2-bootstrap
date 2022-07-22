f078786c0b60bcdba885d91d3d911ab552c8aee0
Gabor Szarnyas <szarnyasg@gmail.com>
2022-07-22 16:23:27 +0200

    Fix sbt install

--- install-packages.sh
+++ install-packages.sh
@@ -3,6 +3,10 @@
 set -eu
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+sudo rm -f /etc/yum.repos.d/bintray-rpm.repo
+curl -L https://www.scala-sbt.org/sbt-rpm.repo > sbt-rpm.repo
+sudo mv sbt-rpm.repo /etc/yum.repos.d/
+
 sudo yum upgrade -y
 sudo yum install -y sbt mc the_silver_searcher nmon bmon fzf sysstat tree nano m4 make cmake automake gcc gcc-c++ kernel-devel zip bc exa bat tldr python3-devel
 
