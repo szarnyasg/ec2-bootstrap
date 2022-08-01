adb9e77847375002775036b97cecbce13920f8aa
David Puroja <dpuroja@outlook.com>
2022-08-01 10:07:14 +0200

    Add out parameters sut instance

--- terraform/compute/out.tf
+++ terraform/compute/out.tf
@@ -5,3 +5,11 @@ output "driver_public_ip" {
 output "driver_public_dns" {
     value = aws_instance.driver.public_dns
 }
+
+output "sut_public_ip" {
+    value = aws_instance.sut.public_ip
+}
+
+output "sut_public_dns" {
+    value = aws_instance.sut.public_dns
+}
